eec33fc4ef59462310215017f4ced1eca9eb70ea
Denghu-JI <denghu.ji7201@gmail.com>
2023-03-15 12:00:51 +1100

    espresso file search done

--- classification_utility/pysearch_tool/pysearch.py
+++ classification_utility/pysearch_tool/pysearch.py
@@ -6,8 +6,10 @@ import os
 
 #please specify cofi methods folder!
 methods_path = "cofi/src/cofi/tools"
-applications_path = "espresso/..."
+applications_path = "espresso/contrib"
 problems_path = "cofi-examples/..."
+
+app_info_name = 'README.md'
 
 
 class pysearch:
@@ -17,7 +19,7 @@ class pysearch:
         first line.
 
         Parameters
-
+        ----------------
         method_path : str
             the folder that contains inference methods in CoFI
         app_path : str
@@ -32,22 +34,36 @@ class pysearch:
 
     def search(self):
         print('loading methods!')
+        #inference methods in cofi
         for _, _, files in os.walk(self._method_path):
             for method in files[:-2]:
                 r = open(methods_path + '/' + method)
+                #current plan, read the hierarchial information in the first line
                 print(method)
                 print(r.readline().strip('\n'))
-                
+
+        #applications in espresso
+        for root, dirs, files in os.walk(self._app_path):
+            if root == self._app_path:
+                for dir in dirs:
+                    #current plan: read the hierarchial info in a file
+                    r = open(self._app_path + '/' + dir + '/' + app_info_name)
+                    print(r.read())
+        
+        
+
+            
 
 
-#----git sync-------------------------
-# path : current path 
-# git_path : root project git path
-path = pathlib.Path().resolve()
-git_path = '/'.join(str(path).split('/')[:-2])
-repo = git.Repo(git_path)
-for submodule in repo.submodules:
-    submodule.update(init=True)
+
+# #----git sync-------------------------
+# # path : current path 
+# # git_path : root project git path
+# path = pathlib.Path().resolve()
+# git_path = '/'.join(str(path).split('/')[:-2])
+# repo = git.Repo(git_path)
+# for submodule in repo.submodules:
+#     submodule.update(init=True)
 #---------------------------------------
 
 #path to methods. applications and problems
